31c6ffb346473a679c78f825cbf53fd51920d13f
Lucas Gabriel Bubanz <109542664+LucasGabriel18@users.noreply.github.com>
2022-07-19 19:12:49 -0300

    finalizei a pagina de login
    Ajustes e finalização da pag de login
    (html, css e js)

--- projeto/php/conexao.php
+++ projeto/php/conexao.php
@@ -8,7 +8,5 @@ $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 
 if($mysqli -> connect_errno) {
     echo "Falha ao conectar com o banco: ( . $mysqli->connect_errno . )" . $mysqli->connect_errno;
-} else{
-    echo "Conectado!";
 }
 ?>
--- projeto/php/index.php
+++ projeto/php/index.php
@@ -7,26 +7,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="../css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>LOGIN</title>
 </head>
 <body>
     <div class="container">
-            <h1 style="text-align: center; justify-content: center; font-size: 25px; font-family:'Times New Roman', Times, serif;">Car Ticket ⚙️</h1><br><br>
-            <h3 style="text-align: center; justify-content: center; font-size: 25px; font-family:'Times New Roman', Times, serif;">LOGIN</h3>
-            <form action="php/verify.php" method="post">
+            <h1 style="text-align: center; justify-content: center; font-size: 25px; font-family:'Times New Roman', Times, serif;">Car Ticket ⚙️</h1><br>
+            <form action="php/cadastro.php" method="post">
                 <div class="input-icon">
                     <strong><label for="email" id="labelEmail">Digite o seu e-mail:</label></strong>
                     <i class="fa fa-user icon"></i>
-                    <input type="text" name="email" id="email" name="email" autofocus required><br>
+                    <input type="text" name="login" id="login" name="login" autofocus required><br>
                     <strong><label for="password" id="labelPassword">Digite sua senha:</label></strong>
                     <i class="fa fa-lock icon"></i> 
                     <input type="password" name="senha" id="senha" required><br>
                     <button type="submit" id="button">Logar</button>
                 </div>
             </form>
-            <script src="js/login.js"></script>
+            <script src="../js/login.js"></script>
     </div>
 </body>
 </html>
