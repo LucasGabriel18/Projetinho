cf47f77391668224c5495056ab982636bcd23da1
Lucas Gabriel Bubanz <109542664+LucasGabriel18@users.noreply.github.com>
2022-07-20 13:27:36 -0300

    update da pag home
    criação da (NavBar, Titulo e SubTitulo com imagem ).

--- projeto/php/home.php
+++ projeto/php/home.php
@@ -5,15 +5,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../css/home.css">
     <title>Home</title>
 </head>
 <body>
     <header class="container">
+        <div class="topnav">
+            <script src="../js/navbar.js"></script>
+            <link rel="stylesheet" href="../css/navbar.css">
+            <a href="home.php" class="active">Home</a>
+            <a href="#news">Nossa história</a>
+            <a href="#contact">Suporte</a>
+            <a href="index.php">Logout</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
         <div class="logo">
             <img src="../imagens/autocar.png" alt="foto">
             <h2>CAR TICKET</h2>
         </div>
     </header>
+    <section class="sessao-1 container">
+        <div class="cardleft">
+            <img src="../imagens/car_left.png" alt="foto">
+        </div>
+        <div class="cardright">
+            <h2>Seja Bem-Vindo ao meu site.</h2>
+            <p>Aqui você pode cadastrar seu veículo de um forma segura e rápida.</p>
+        </div>
+    </section>
 </body>
 </html>
