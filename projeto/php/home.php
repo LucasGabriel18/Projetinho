58010ce245d400826cf3ede192ed3d02963a7e64
Lucas Gabriel Bubanz <109542664+LucasGabriel18@users.noreply.github.com>
2022-07-20 11:55:56 -0300

    ESTILIZAÇÃO DO HEADER
    criação do titulo e do logo

---
+++ projeto/php/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/home.css">
+    <title>Home</title>
+</head>
+<body>
+    <header class="container">
+        <div class="logo">
+            <img src="../imagens/autocar.png" alt="foto">
+            <h2>CAR TICKET</h2>
+        </div>
+    </header>
+</body>
+</html>
